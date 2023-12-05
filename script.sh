b9a0c79d67e298b5f13e62ba40521d6221b2efd9
mitrasonal <mitrasonal130996@gmail.com>
2023-12-05 21:06:05 +0530

    add script file

--- script.sh
+++ script.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 apt-get update -y
 apt-get install nginx -y
-cd /var/www/html
-wget https://www.free-css.com/assets/files/free-css-templates/download/page296/healet.zip
-apt install unziip -y
-unzip healet.zip
+cp -r * /var/www/html
