6fa4bf657201793682916dfad778d252d55d2aa2
mitrasonal <mitrasonal130996@gmail.com>
2023-12-05 20:58:19 +0530

    add script file

--- script.sh
+++ script.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-
-echo "my repo 1"
-
+apt-get update -y
+apt-get install nginx -y
+cd /var/www/html
+wget https://www.free-css.com/assets/files/free-css-templates/download/page296/healet.zip
+apt install unziip -y
+unzip healet.zip
